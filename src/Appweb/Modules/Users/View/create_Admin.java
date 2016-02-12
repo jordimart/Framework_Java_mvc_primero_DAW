f414fcba99e8143e8acb237448953bf90891d4ff
Jorge M <jordimart83@gmail.com>
2016-02-13 00:09:21 +0100

    Cambit directoris de alguns look and feels

--- src/Appweb/Modules/Users/View/create_Admin.java
+++ src/Appweb/Modules/Users/View/create_Admin.java
@@ -242,7 +242,6 @@ public class create_Admin extends javax.swing.JFrame {
                         .addGroup(panelCreateAdmin1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(jLabel11)
                             .addComponent(jDateChooser1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(panelCreateAdmin1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jLabel12)
                     .addComponent(jTextField12, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
@@ -303,29 +302,7 @@ public class create_Admin extends javax.swing.JFrame {
             jLblFoto.setSize(470,290);
     }//GEN-LAST:event_btnImagenAActionPerformed
     }
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) throws ParseException {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-               UIManager.setLookAndFeel(new SyntheticaBlackEyeLookAndFeel() {});
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(create_Admin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new create_Admin().setVisible(true);
-            }
-        });
-    }
+    /**/
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnImagenA;
